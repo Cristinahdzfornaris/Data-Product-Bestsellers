78108c8e91b1746e79b56c1d82312eaa3ba13781
Cristinahdzfornaris <cristihf05.gmail.com>
2024-08-16 21:34:17 -0400

    .

--- dataproductoficial.py
+++ dataproductoficial.py
@@ -7,7 +7,19 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 from streamlit.components.v1 import html
+ga_code = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KYT9QJN6FW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-KYT9QJN6FW');
+</script>
+"""
+
+html(ga_code, height=0)
 # Configurar estilos CSS
 st.markdown("""
     <style>
@@ -65,19 +77,7 @@ st.markdown("""
         }
     </style>
 """, unsafe_allow_html=True)
-ga_code = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-KYT9QJN6FW"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-KYT9QJN6FW');
-</script>
-"""
-
-html(ga_code, height=0)
 # Crear una función para cargar los datos
 @st.cache_data
 def cargar_datos(file_path):
