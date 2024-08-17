88115da1bc6d5512ea6eb4dd39d9cfc17e4239f7
Cristinahdzfornaris <cristihf05.gmail.com>
2024-08-16 20:08:52 -0400

    .

--- dataproductoficial.py
+++ dataproductoficial.py
@@ -68,8 +68,8 @@ st.markdown("""
 # Crear una función para cargar los datos
 @st.cache_data
 def cargar_datos(file_path):
-    datos = pd.read_csv(file_path)
-    st.write(datos.columns)
+    datos = pd.read_csv(file_path, )
+    
     datos['bestsellers_date'] = pd.to_datetime(datos['bestsellers_date'], format='%Y-%m-%d')
     datos['published_date'] = pd.to_datetime(datos['published_date'], format='%Y-%m-%d')
     datos['month'] = datos['bestsellers_date'].dt.month
@@ -82,7 +82,7 @@ def cargar_datos(file_path):
     return datos
 
 # Cargar datos
-ruta= 'Datos\datos2013-2023.csv'
+ruta= 'Datos/datos2013-2023.csv'
 datos = cargar_datos(ruta)
 
 
